3448d136642889a4cdf0d69050464cf7b8b16da4
Yao Cheng <yaochengchn@163.com>
2018-09-26 16:13:41 +0800

    1.增加sto_industry函数，调用该函数可以得到股票的行业分类； 2. 微调其他内容

--- get_stock.R
+++ get_stock.R
@@ -6,6 +6,7 @@ require(stringr)
 require(lubridate)
 require(quantmod)
 require(xts)
+require(WindR)
 
 
 
@@ -78,20 +79,23 @@ require(xts)
         
         }
         
-# sto_name 用于将股票代码转换为公司名称，例如将“600030”或“600030.sh”转换为中信证券#######
+# sto_name 用于将股票代码转换为公司名称，例如将“600030”或“600030.sh”转换为"中信证券"#######
         sto_name <- function(code){
         ## code: 【字符串】，表示股票代码，只有字符串的前六位数字会被截取
                 
         # 从data文件夹中获取数据文件
                 sto_name <- read_csv("data/stock_names.csv")
                 
+        # 去掉代码中的“ST”
+                if (str_detect(code, "^[Ss][Tt]"))
+                        code <- str_sub(code,3)
+                
         # 截取code的前6位
                 code6 <- str_sub(code,1,6)
                 
         # code前六位是否是数字？
                 if(!str_detect(code6, "\\d{6}")){
-                        print("请确保输入代码的前6位是数字")
-                        return()
+                        invisible(stop("请确保输入代码的前6位是数字"))
                 }
         # 哪些行匹配？ 
                 good <- str_detect(sto_name[[3]], str_c("^",code6,"$"))
@@ -103,7 +107,7 @@ require(xts)
         
 # sto_code 用于将公司名称转换为代码######
         sto_code <- function(stockname, code_style = "wind", exact = TRUE){
-        ## stockname：【字符串】，表示股票的中文名称；
+        ## stockname：【字符串】表示股票的中文名称；
         ## code_style 【字符串】表示生成的代码类型，共有三种：
         ##      1. pure： 六位数字
         ##      2. wind： wind代码
@@ -123,4 +127,55 @@ require(xts)
         
         # 获取符合条件的股票名称
                 sto_name[[str_c(code_style,"_name")]][good]
-}
+        }
+       
+              
+        
+# sto_industry 用于将公司名称或者公司代码转换成行业########################
+        sto_industry <- function(stock, classbrand ="sw",level = 1){
+        ## stock: 【字符串】表示股票的中文名称或者股票代码。若是股票代码，只有字符串的前6位会被截取
+        ## classbrand: 【字符串】表示颁布行业分类的机构。包括：
+                # 1. sw：申万
+                # 2. zjh：证监会
+                # 3. zx：中信
+                # 4. wind： 万得
+        ## level：【数字】表示分类级别。1、2、3分别表示1、2、3级，0表示全部
+                
+                
+        # 如果传入的前6位是数字，则将其转化为股票名称
+                
+                # 去掉代码中的“ST”
+                if (str_detect(stock, "^[Ss][Tt]"))
+                        stock <- str_sub(stock,3)
+                
+                # 截取stock的前6位
+                code6 <- str_sub(stock,1,6)
+                
+                # 如果stock的前6位是代码，则将stock变量转化为中文股票名
+                if (str_detect(code6,"\\d{6}"))
+                stock <- sto_name(code6)
+                
+                
+                # 获取股票wind代码
+                stock <- sto_code(stock)
+                
+                # 打开windR，并获取股票所属行业
+                w.start(showmenu = FALSE)
+                
+                if(classbrand=="sw")
+                        brand = "industry_sw"
+                else if(classbrand=="zjh")
+                        brand = "industry_CSRC12"
+                else if(classbrand=="zx")
+                        brand = "industry_citic"
+                else if(classbrand=="wind")
+                        brand = "industry_gics"
+                else 
+                        invisible(stop("请输入正确的分类标准"))
+                
+                outp <-w.wsd(stock, brand, today(),today(),str_c("industryType=", level))
+                outp$Data[[2]]
+        }
+        
+        
+###################################以下为内部函数###########################################################
